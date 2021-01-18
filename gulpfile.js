5d9d8e1ad41ee7952ff3a148c6eba04e5576fb96
Santiago Quintero <quinterosantiago15@gmail.com>
2021-01-18 13:07:46 -0500

    adding setting for the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,17 @@ const { series, src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
 const notify = require('gulp-notify');
+const webp = require('gulp-webp');
+
+const paths = {
+    images: './src/img/**/*',
+    scss: './src/scss/**/*.scss'
+}
 
 // función que compila SASS
 function css() {
     // src --> busca el archivo a compilar
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         // compilar a sass
         .pipe(sass({
             outputStyle: 'expanded'
@@ -16,7 +22,7 @@ function css() {
 }
 
 function minificarcss() {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         // compilar a sass
         .pipe(sass({
             outputStyle: 'compressed'
@@ -27,7 +33,7 @@ function minificarcss() {
 
 // minificar imagenes
 function images() {
-    return src('./src/img/**/*')
+    return src(paths.images)
         .pipe(imagemin())
         // destino donde van a quedar las imagenes minificadas
         .pipe(dest('./build/img'))
@@ -35,15 +41,23 @@ function images() {
         .pipe(notify({message: 'Imagen minificada'}));
 }
 
+// webp -- convertir las images a webp
+function versionWebp() {
+    return src(paths.images)
+        .pipe(webp())
+        .pipe(dest('./build/img'))
+        .pipe(notify({message: 'Versión webp lista'}));
+}
+
 // escucha los cambios que van teniendo los archivos
 function watchFiles() {
     // * = la carpeta actual
     // ** = todos los archivos con esas extensión
-    watch('src/scss/**/*.scss', css);
+    watch(paths.scss, css);
 }
 
 exports.watchFiles = watchFiles;
 exports.css = css;
 exports.minificarcss = minificarcss;
 exports.images = images;
-exports.default = series( css, images, watchFiles);
+exports.default = series( css, images, versionWebp, watchFiles);
